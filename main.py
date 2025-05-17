b494d1356e06b3cba7499c46f2fc06875a4a177f
scarowar <skanthavar@live.com>
2025-05-17 12:58:13 +0530

    feat(app) add player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,12 +10,32 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
+            
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
+        for item in updatable:
+            item.update(dt)
 
 if __name__ == "__main__":
     main()
